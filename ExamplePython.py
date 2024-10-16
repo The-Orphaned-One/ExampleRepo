dc42b65d6a63056606d1ebb8e9250b129d3551ba
The-Orphaned-One <Jtrevino12315@gmail.com>
2024-10-16 13:47:42 -0500

    Update ExamplePython.py
    Password Deleted

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,3 +1,4 @@
+
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
@@ -12,5 +13,6 @@ print(f"x = {x} and y = {y}")
 print("c = {c} and d = {d}".format(c=1, d=2))
 
 job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+
 for name, job in job_list.items ():
     print(f"{name} is a {job}")
